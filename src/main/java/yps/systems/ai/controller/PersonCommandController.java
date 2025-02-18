52d79d8d8b624ac7900802ee9eb8acc99b600cbc
ShanderSandoval <135709230+ShanderSandoval@users.noreply.github.com>
2025-02-18 12:29:43 -0500

    Update PersonCommandController.java

--- src/main/java/yps/systems/ai/controller/PersonCommandController.java
+++ src/main/java/yps/systems/ai/controller/PersonCommandController.java
@@ -14,6 +14,7 @@ import yps.systems.ai.repository.IPersonRepository;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/command/personService")
 public class PersonCommandController {
